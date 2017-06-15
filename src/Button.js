652f08fcd16ad77b0d97faa749a17299c918135f
chelle tuerk <michelletuerk@gmail.com>
2017-06-14 23:45:12 -0600

    add calculate button, refactor button component

---
+++ src/Button.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Button = (props) => {
+  return (
+  <button
+    onClick={props.handleClick}
+    className={props.className}>{props.text}
+  </button>
+  )
+}
+
+export default Button
