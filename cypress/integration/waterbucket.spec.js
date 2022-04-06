d1b531f65b9c287c748fe98a1183fcb9697077cd
chelle tuerk <tuerkbot@talkingRobot.local>
2022-04-05 18:36:59 -0700

    fix yml file name

--- cypress/integration/waterbucket.spec.js
+++ cypress/integration/waterbucket.spec.js
@@ -1,5 +1,5 @@
 describe('My First Test', () => {
-  it('Doesnt do much, but everything it needs to!', () => {
+  it('Doesnt do much, but everything it should be!', () => {
     expect(true).to.equal(true)
   })
 })
